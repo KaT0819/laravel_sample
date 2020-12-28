d981fc2d1026660b7acfd9625b9e863061e10e03
KatsuyukiToki <katuyuki.toki@gmail.com>
2020-12-29 01:17:58 +0900

    ・Laravel Mix導入 ・profile編集

--- server/sample/resources/views/admin/profile/create.blade.php
+++ server/sample/resources/views/admin/profile/create.blade.php
@@ -1,11 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Myプロフィール</title>
-    </head>
-    <body>
-        <h1>Myプロフィール作成画面</h1>
-    </body>
-</html>
+@extends('layouts.profile')
+
+@section('title', 'プロフィールの新規作成')
+
+@section('content')
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 mx-auto">
+                    <h2>プロフィール新規作成</h2>
+                </div>
+            </div>
+        </div>
+@endsection
--- server/sample/resources/views/admin/profile/edit.blade.php
+++ server/sample/resources/views/admin/profile/edit.blade.php
@@ -1,11 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Myプロフィール</title>
-    </head>
-    <body>
-        <h1>Myプロフィール編集画面</h1>
-    </body>
-</html>
+@extends('layouts.profile')
+
+@section('title', 'プロフィールの編集')
+
+@section('content')
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 mx-auto">
+                    <h2>プロフィール編集</h2>
+                </div>
+            </div>
+        </div>
+@endsection
