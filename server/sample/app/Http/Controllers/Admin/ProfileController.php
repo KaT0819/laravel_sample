8d956cbcf5490856fc26d8dbb4ef14ae258ba9fb
KatsuyukiToki <katuyuki.toki@gmail.com>
2020-12-31 00:40:50 +0900

    登録処理実装

--- server/sample/app/Http/Controllers/Admin/ProfileController.php
+++ server/sample/app/Http/Controllers/Admin/ProfileController.php
@@ -5,13 +5,30 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profile;
+
 class ProfileController extends Controller
 {
     public function add() {
         return view('admin.profile.create');
     }
 
-    public function create() {
+    public function create(Request $request) {
+        // Varidationを行う
+        $this->validate($request, Profile::$rules);
+
+        $profile = new Profile;
+        $form = $request->all();
+
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        // フォームから送信されてきたimageを削除する
+        unset($form['image']);
+
+        // データベースに保存する
+        $profile->fill($form);
+        $profile->save();
+
         return redirect('admin/profile/create');
     }
 
@@ -19,7 +36,7 @@ class ProfileController extends Controller
         return view('admin.profile.edit');
     }
 
-    public function update() {
+    public function update(Request $request) {
         return redirect('admin/profile/edit');
     }
 
