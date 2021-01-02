1a7df3152e4121e7eae203f90504cff5c0c4e1af
KatsuyukiToki <katuyuki.toki@gmail.com>
2021-01-02 11:37:08 +0900

    ・typo修正 ・教材不備。bladeでStr::はLaravel5では使用できず、フルパス指定とした。6以降からサポートされているはずなので使用可能なはず

--- server/sample/resources/views/admin/news/index.blade.php
+++ server/sample/resources/views/admin/news/index.blade.php
@@ -41,8 +41,8 @@
                             @foreach($posts as $news)
                                 <tr>
                                     <td>{{ $news->id }}</td>
-                                    <td>{{ \Str::limit($news->title, 100) }}</td>
-                                    <td>{{ \Str::limit($news->body, 250) }}</td>
+                                    <td>{{ Illuminate\Support\Str::limit($news->title, 100) }}</td>
+                                    <td>{{ Illuminate\Support\Str::limit($news->body, 250) }}</td>
                                     <td>
                                         <div>
                                             <a href="{{ action('Admin\NewsController@edit', ['id' => $news->id]) }}">編集</a>
